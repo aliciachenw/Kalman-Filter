ffcd84642041a4dabfe5235cc7a4af36d7ffef68
Wanwen Chen <aliciawwchen@gmail.com>
2019-02-26 09:17:24 +0800

    Update Euler2Qua.m

--- Euler2Qua.m
+++ Euler2Qua.m
@@ -1,4 +1,5 @@
 function q=Euler2Qua(yaw,pitch,roll)
+% X-pitch Y-roll Z-yaw
 q=zeros(4,1);
 q(1)=sin(pitch/2)*sin(roll/2)*sin(yaw/2)+cos(pitch/2)*cos(roll/2)*cos(yaw/2);
 q(2)=sin(roll/2)*cos(pitch/2)*cos(yaw/2)-sin(pitch/2)*sin(yaw/2)*cos(roll/2);
